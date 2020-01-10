19a4d30b881b20c9b687c18afc1f371dd18ff431
Dmitry <dmitry.surkin@gmail.com>
2020-01-10 16:53:40 +0500

    Доделываю слайдер

--- script.js
+++ script.js
@@ -1,85 +1,63 @@
-const buttonChooseImg = document.querySelectorAll('.product-controls-list__item');
+"use strict";
+
+const buttonsChooseImg = document.querySelectorAll('.product-controls-list__item');
 const colorChooseImg = document.querySelectorAll('.color-controls-list__item');
-const buttonChooseImgArrow = document.querySelectorAll('.product-controls__button');
+const buttonsChooseImgArrow = document.querySelectorAll('.product-controls__button');
 const sliderList = document.querySelector('.main-photo');
 
-for (let elem of buttonChooseImg) {
+for (let elem of buttonsChooseImg) {
     elem.addEventListener('click', function () {
-        const mainPhoto = searchMainPhoto(elem);
-        toogleActivePhoto(mainPhoto);
-        removeSelectedButton(elem);
-        elem.classList.add('js-active');
-    });
-}
+        removePreviousPhoto();
+        removePreviousButton();
+        addActiveElements(elem);
+    })
+};
+
+for (let elem of buttonsChooseImgArrow) {
+    elem.addEventListener('click', function () {
+        const activeButton = document.querySelector('.js-active');
+        const previousSibling = activeButton.previousElementSibling;
+        const nextSibling = activeButton.nextElementSibling;
+
+        //Если нажали на стрелку вверх у которой есть предыдущий элемент
+        if (event.currentTarget == buttonsChooseImgArrow[0] && previousSibling) {
+            removePreviousButton();
+            removePreviousPhoto();
+            addActiveElements(previousSibling);
+        }
+
+        //В противном случае если нажали на стрелку вниз у которой есть следующий элемент
+        else if (event.currentTarget == buttonsChooseImgArrow[1] && nextSibling) {
+            removePreviousButton();
+            removePreviousPhoto();
+            addActiveElements(nextSibling);
+        }
+    })
+};
 
 for (let elem of colorChooseImg) {
-    elem.addEventListener('click', function(event) {
-        const index = elem.getAttribute('data-img');
-        const selectedMainPhoto = sliderList.querySelector('.js_display_block');
-
-        if (index == 1) {
-            selectedMainPhoto.style.opacity = 0.9;
-        };
-        if (index == 2) {
-            selectedMainPhoto.style.opacity = 0.8;
-        };
-        if (index == 3) {
-            selectedMainPhoto.style.opacity = 0.7;
-        }; 
-        if (index == 4) {
-            selectedMainPhoto.style.opacity = 0.6;
-        }; 
-        if (index == 5) {
-            selectedMainPhoto.style.opacity = 0.5;
-        };
+    elem.addEventListener('click', function () {
+        const color = elem.getAttribute('data-color');
+        const activePhoto = sliderList.querySelector('.js_display_block');
+        activePhoto.style.boxShadow = '0 0 10px 20px ' + color;
     })
 }
 
-for (let elem of buttonChooseImgArrow) {
-    elem.addEventListener('click', function(event) {
-        const selectedButtonChooseImg = document.querySelector('.js-active');
-        
-        if (event.currentTarget == buttonChooseImgArrow[0] && (selectedButtonChooseImg !== buttonChooseImg[0])) {
-            selectedButtonChooseImg.classList.remove('js-active');
-            const previousSibling = selectedButtonChooseImg.previousElementSibling;
-            previousSibling.classList.add('js-active');
-
-            const mainPhoto = searchMainPhoto(previousSibling);
-            toogleActivePhoto(mainPhoto);
-        }
-        
-        if (event.currentTarget == buttonChooseImgArrow[1] && (selectedButtonChooseImg !== buttonChooseImg[buttonChooseImg.length-1])) {
-            selectedButtonChooseImg.classList.remove('js-active');
-            const nextSibling = selectedButtonChooseImg.nextElementSibling;
-            nextSibling.classList.add('js-active');
-            
-            const mainPhoto = searchMainPhoto(nextSibling);
-            toogleActivePhoto(mainPhoto);
-        }
-    })
+function removePreviousPhoto() {
+    const activePhoto = sliderList.querySelector('.js_display_block');
+    activePhoto.classList.remove('js_display_block');
 }
 
+function removePreviousButton() {
+    const activeButton = document.querySelector('.js-active');
+    activeButton.classList.remove('js-active');
+    const index = activeButton.getAttribute('data-img');
+}
 
-//Находит нужное изображение к нажатой кнопке
-function searchMainPhoto(elem) {
+function addActiveElements(elem) {
+    elem.classList.add('js-active');
     const index = elem.getAttribute('data-img');
-    const mainPhoto = sliderList.children[index - 1];
 
-    return mainPhoto;
-};
-
-//Переключает старое и новое изображение
-function toogleActivePhoto(mainPhoto) {
-    const selectedMainPhoto = sliderList.querySelector('.js_display_block');
-    selectedMainPhoto.classList.remove('js_display_block');
-    mainPhoto.classList.add('js_display_block');
-};
-
-//Удаляет выделение у кнопки
-function removeSelectedButton(elem) {
-    const selectedButtonChooseImg = document.querySelector('.js-active');
-    selectedButtonChooseImg.classList.remove('js-active');
+    const needPhoto = sliderList.children[index];
+    needPhoto.classList.add('js_display_block');
 }
-
-
-
